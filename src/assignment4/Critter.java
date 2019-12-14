5c761640aa058bbd46f438b7373885d2e53f7a32
Zane Freeman <36860193+thecartersue@users.noreply.github.com>
2019-12-13 22:47:03 -0600

    Add files via upload

---
+++ src/assignment4/Critter.java
@@ -0,0 +1,208 @@
+/*
+ * CRITTERS Critter.java
+ * EE422C Project 4 submission by
+ * Replace <...> with your actual data.
+ * <Student1 Name>
+ * <Student1 EID>
+ * <Student1 5-digit Unique No.>
+ * <Student2 Name>
+ * <Student2 EID>
+ * <Student2 5-digit Unique No.>
+ * Slip days used: <0>
+ * Spring 2019
+ */
+
+package assignment4;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+/* 
+ * See the PDF for descriptions of the methods and fields in this
+ * class. 
+ * You may add fields, methods or inner classes to Critter ONLY
+ * if you make your additions private; no new public, protected or
+ * default-package code or data can be added to Critter.
+ */
+
+public abstract class Critter {
+
+    private int energy = 0;
+
+    private int x_coord;
+    private int y_coord;
+
+    private static List<Critter> population = new ArrayList<Critter>();
+    private static List<Critter> babies = new ArrayList<Critter>();
+
+    /* Gets the package name.  This assumes that Critter and its
+     * subclasses are all in the same package. */
+    private static String myPackage;
+
+    static {
+        myPackage = Critter.class.getPackage().toString().split(" ")[1];
+    }
+
+    private static Random rand = new Random();
+
+    public static int getRandomInt(int max) {
+        return rand.nextInt(max);
+    }
+
+    public static void setSeed(long new_seed) {
+        rand = new Random(new_seed);
+    }
+
+    /**
+     * create and initialize a Critter subclass.
+     * critter_class_name must be the qualified name of a concrete
+     * subclass of Critter, if not, an InvalidCritterException must be
+     * thrown.
+     *
+     * @param critter_class_name
+     * @throws InvalidCritterException
+     */
+    public static void createCritter(String critter_class_name)
+            throws InvalidCritterException {
+        // TODO: Complete this method
+    }
+
+    /**
+     * Gets a list of critters of a specific type.
+     *
+     * @param critter_class_name What kind of Critter is to be listed.
+     *        Unqualified class name.
+     * @return List of Critters.
+     * @throws InvalidCritterException
+     */
+    public static List<Critter> getInstances(String critter_class_name)
+            throws InvalidCritterException {
+        // TODO: Complete this method
+        return null;
+    }
+
+    /**
+     * Clear the world of all critters, dead and alive
+     */
+    public static void clearWorld() {
+        // TODO: Complete this method
+    }
+
+    public static void worldTimeStep() {
+        // TODO: Complete this method
+    }
+
+    public static void displayWorld() {
+        // TODO: Complete this method
+    }
+
+    /**
+     * Prints out how many Critters of each type there are on the
+     * board.
+     *
+     * @param critters List of Critters.
+     */
+    public static void runStats(List<Critter> critters) {
+        System.out.print("" + critters.size() + " critters as follows -- ");
+        Map<String, Integer> critter_count = new HashMap<String, Integer>();
+        for (Critter crit : critters) {
+            String crit_string = crit.toString();
+            critter_count.put(crit_string,
+                    critter_count.getOrDefault(crit_string, 0) + 1);
+        }
+        String prefix = "";
+        for (String s : critter_count.keySet()) {
+            System.out.print(prefix + s + ":" + critter_count.get(s));
+            prefix = ", ";
+        }
+        System.out.println();
+    }
+
+    public abstract void doTimeStep();
+
+    public abstract boolean fight(String oponent);
+
+    /* a one-character long string that visually depicts your critter
+     * in the ASCII interface */
+    public String toString() {
+        return "";
+    }
+
+    protected int getEnergy() {
+        return energy;
+    }
+
+    protected final void walk(int direction) {
+        // TODO: Complete this method
+    }
+
+    protected final void run(int direction) {
+        // TODO: Complete this method
+
+    }
+
+    protected final void reproduce(Critter offspring, int direction) {
+        // TODO: Complete this method
+    }
+
+    /**
+     * The TestCritter class allows some critters to "cheat". If you
+     * want to create tests of your Critter model, you can create
+     * subclasses of this class and then use the setter functions
+     * contained here.
+     * <p>
+     * NOTE: you must make sure that the setter functions work with
+     * your implementation of Critter. That means, if you're recording
+     * the positions of your critters using some sort of external grid
+     * or some other data structure in addition to the x_coord and
+     * y_coord functions, then you MUST update these setter functions
+     * so that they correctly update your grid/data structure.
+     */
+    static abstract class TestCritter extends Critter {
+
+        protected void setEnergy(int new_energy_value) {
+            super.energy = new_energy_value;
+        }
+
+        protected void setX_coord(int new_x_coord) {
+            super.x_coord = new_x_coord;
+        }
+
+        protected void setY_coord(int new_y_coord) {
+            super.y_coord = new_y_coord;
+        }
+
+        protected int getX_coord() {
+            return super.x_coord;
+        }
+
+        protected int getY_coord() {
+            return super.y_coord;
+        }
+
+        /**
+         * This method getPopulation has to be modified by you if you
+         * are not using the population ArrayList that has been
+         * provided in the starter code.  In any case, it has to be
+         * implemented for grading tests to work.
+         */
+        protected static List<Critter> getPopulation() {
+            return population;
+        }
+
+        /**
+         * This method getBabies has to be modified by you if you are
+         * not using the babies ArrayList that has been provided in
+         * the starter code.  In any case, it has to be implemented
+         * for grading tests to work.  Babies should be added to the
+         * general population at either the beginning OR the end of
+         * every timestep.
+         */
+        protected static List<Critter> getBabies() {
+            return babies;
+        }
+    }
+}
---
+++ src/assignment4/Main.java
@@ -0,0 +1,101 @@
+/*
+ * CRITTERS Main.java
+ * EE422C Project 4 submission by
+ * Replace <...> with your actual data.
+ * <Student1 Name>
+ * <Student1 EID>
+ * <Student1 5-digit Unique No.>
+ * <Student2 Name>
+ * <Student2 EID>
+ * <Student2 5-digit Unique No.>
+ * Slip days used: <0>
+ * Spring 2019
+ */
+
+package assignment4;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+import java.util.Scanner;
+
+/*
+ * Usage: java <pkg name>.Main <input file> test input file is
+ * optional.  If input file is specified, the word 'test' is optional.
+ * May not use 'test' argument without specifying input file.
+ */
+
+public class Main {
+
+    /* Scanner connected to keyboard input, or input file */
+    static Scanner kb;
+
+    /* Input file, used instead of keyboard input if specified */
+    private static String inputFile;
+
+    /* If test specified, holds all console output */
+    static ByteArrayOutputStream testOutputString;
+
+    /* Use it or not, as you wish! */
+    private static boolean DEBUG = false;
+
+    /* if you want to restore output to console */
+    static PrintStream old = System.out;
+
+    /* Gets the package name.  The usage assumes that Critter and its
+       subclasses are all in the same package. */
+    private static String myPackage; // package of Critter file.
+
+    /* Critter cannot be in default pkg. */
+    static {
+        myPackage = Critter.class.getPackage().toString().split(" ")[1];
+    }
+
+    /**
+     * Main method.
+     *
+     * @param args args can be empty.  If not empty, provide two
+     *             parameters -- the first is a file name, and the
+     *             second is test (for test output, where all output
+     *             to be directed to a String), or nothing.
+     */
+    public static void main(String[] args) {
+        if (args.length != 0) {
+            try {
+                inputFile = args[0];
+                kb = new Scanner(new File(inputFile));
+            } catch (FileNotFoundException e) {
+                System.out.println("USAGE: java <pkg name>.Main OR java <pkg name>.Main <input file> <test output>");
+                e.printStackTrace();
+            } catch (NullPointerException e) {
+                System.out.println("USAGE: java <pkg name>.Main OR java <pkg name>.Main <input file> <test output>");
+            }
+            if (args.length >= 2) {
+                /* If the word "test" is the second argument to java */
+                if (args[1].equals("test")) {
+                    /* Create a stream to hold the output */
+                    testOutputString = new ByteArrayOutputStream();
+                    PrintStream ps = new PrintStream(testOutputString);
+                    /* Save the old System.out. */
+                    old = System.out;
+                    /* Tell Java to use the special stream; all
+                     * console output will be redirected here from
+                     * now */
+                    System.setOut(ps);
+                }
+            }
+        } else { // If no arguments to main
+            kb = new Scanner(System.in); // Use keyboard and console
+        }
+        commandInterpreter(kb);
+
+        System.out.flush();
+    }
+
+    /* Do not alter the code above for your submission. */
+
+    private static void commandInterpreter(Scanner kb) {
+        //TODO Implement this method
+    }
+}
